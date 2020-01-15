4f8ac9f9945f407fc116cb35ce3ecc69f2e72037
Sawyer Fisher <sawyerkove@gmail.com>
2020-01-15 17:27:21 -0600

    html bones with some css

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+var questions = [
+    {   
+        title: "What is the HTML tag under which one can write the JavaScript code?",
+        choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "Which of the following is the correct syntax to display “I love JS” in an alert box using JavaScript?",
+        choices: ["alertbox('I love JS');", "msg('I love JS');", "<msgbox('I love JS');", "alert('I love JS');"],
+        answer: "alert('I love JS');" 
+    },
+    {
+        title: "What is the correct syntax for referring to an external script called “script.js”?",
+        choices: ['<script src=”script.js”>', '<script href=”script.js”>', '<script ref=”script.js”>', '<script name=”script.js”>'],
+        answer: "<script src=”script.js”>"
+    },
+    {
+        title: "The external JavaScript file must contain <script> tag. True or False?",
+        choices: ["True", "False", "Nothing", "Oops"],
+        answer: "False" 
+    },
+    {
+        title: "What is the syntax for creating a function in JavaScript named as myFunc?",
+        choices: ["function = myFunc()", "function myFunc()", "function := myFunc()", "function : myFunc()"],
+        answer: "function myFunc()"
+    }
+];
+
+
+//var startButton = document.createElement('div');
+//startButton.id = 'start-btn';
+
+
+
+//start button
+//document.getElementById("start-btn").addEventListener("click", startQuiz());
+
+
+
+
+function startQuiz() {
+    
+}
+
